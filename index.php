b5a31786bfa5242fc6ff2bce0c137aea05847f62
Brajpal yadav <40633905+brajpal98@users.noreply.github.com>
2019-07-18 09:28:50 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <title>MovieBuff</title>
+  </head>
+  <body>
+    <h1 align="center">MovieBuff</h1>
+    <h3 align="center">Please login to continue...</h3>
+    <tr>
+      <td><input type="text" name="email"></td>
+    </tr>
+    <tr>
+      <td><input type="password" name="password"></td>
+    </tr>
+    <tr>
+      <td><input type="submit" name="submit">
+  </body>
+</html>
